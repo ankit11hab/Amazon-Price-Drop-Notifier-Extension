126870cdd402a8ea888c5d4c869f028e153331a3
Ankit Guha <75986219+ankit11hab@users.noreply.github.com>
2021-07-07 15:12:42 +0530

    Task 5

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 const express = require('express'); // importing express modules
+const mongoose = require('mongoose');
 const app = express();
+
+
+// connect to mongoDB
+const dbURI = 'mongodb+srv://ankit11hab:AnkitGuha@cluster0.uzilj.mongodb.net/AmazonData?retryWrites=true&w=majority'; 
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then((result) =>
+{
+  console.log("Database is connected");
+})
+.catch((error) =>
+{
+  console.log("An error has occured!");
+})
 
 app.get("/", (request,response)=>
 {
@@ -13,6 +27,6 @@ app.post("/", (request, response) => {
 
 app.listen(3000, function()
 {
-    console.log("Server started at port 3000! (Console log through listen function)");
+    console.log("Server started on port 3000!");
 })
 
